71b7d0b3de7c4203469b2793f8e0998e9aaddb1b
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-08-16 01:53:14 +0600

    basic display is added

--- cal24.py
+++ cal24.py
@@ -5,4 +5,11 @@ root = Tk()
 root.title('Calculator')
 root.geometry('280x380')
 root.resizable(0,0)
+# root.config(background='black')
+
+# display section
+display = Label(root,text=0,bg='gray',fg='white')
+display.grid(row=0,column=0,pady=(50,25))
+display.config(font=('verdana',30,'bold'))
+
 root.mainloop()
